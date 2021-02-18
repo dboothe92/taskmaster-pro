59747e11e1c0d04b374e45f64f18e3056f4b6da6
Dan B <dbraunby@hotmail.com>
2021-02-18 13:18:07 -0700

    ability to drag and drop tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,57 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolertance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    console.log("activate", this);
+  }, 
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  }, 
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    //array to store task data
+    let tempArr = [];
+
+    $(this).children().each(function() {
+      let text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      let date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      })
+
+      console.log(text, date);
+    });
+
+    //trim down list's id to match object property
+    let arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
 //On click changes the p to a textarea
 $(".list-group").on("click", "p", function() {
   let text = $(this).text().trim();
@@ -136,8 +187,21 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
-
-
+//trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event) {
+    console.log("over");
+  },
+  out: function(event) {
+    console.log("out");
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
